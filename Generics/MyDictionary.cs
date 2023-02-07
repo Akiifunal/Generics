b9685ccd6ab27bc40f44a4a663313119e49cf199
Akiifunal <akif_unal@icloud.com>
2023-02-07 14:34:16 +0300

    Add project files.

---
+++ Generics/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class MyDictionary<T>
+    {
+        T[] Array;
+        T[] _TempArray;
+        public MyDictionary()
+        {
+            Array = new T[0];
+        }
+
+       
+
+        public void Add(T item)
+        {
+           _TempArray = Array;
+            Array = new T[_TempArray.Length + 1];
+            for (int i = 0; i < _TempArray.Length; i++)
+            {
+                _TempArray[i] = Array[i];
+            }
+            Array[Array.Length - 1] = item;
+
+
+        }
+        public int Length
+        {
+            get { return Array.Length; }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Generics
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            MyDictionary <string> sehirler = new MyDictionary <string>();
+            sehirler.Add("Maraş");
+            sehirler.Add("Antep");
+            sehirler.Add("Hatay");
+            Console.WriteLine(sehirler.Length);
+            Console.WriteLine("Dualarımız sizlerle");
+        }
+    }
+}
+
